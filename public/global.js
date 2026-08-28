6849687343da606b01b87aab6f90e0115a765fbc
Humma <irrelevantreviews.site@gmail.com>
2026-08-29 01:28:23 +0500

    Review: Parappa the Rapper 2

--- public/global.js
+++ public/global.js
@@ -8,7 +8,7 @@ function spoiler(no){
 // random review function
 const reviews = ["2026/rhythm-tengoku", "2026/locoroco", "2026/parappa-the-rapper", "2026/miitopia", "2026/snipperclips",
                "2026/smash-bros-brawl", "2026/katamari-damacy", "2026/gitaroo-man", "2026/tangle-tower", "2026/tomodachi-life-tld",
-               "2026/mermaid-mask", "2026/rhythm-heaven-groove", "2026/we-love-katamari"];
+               "2026/mermaid-mask", "2026/rhythm-heaven-groove", "2026/we-love-katamari", "2026/parappa-the-rapper-2"];
 
 function randomReview(){
     window.open("https://irrelevantreviews.neocities.org/reviews/" + reviews[Math.floor(Math.random() * reviews.length)]);
